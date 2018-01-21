ddb8f942c6d7ff9e32c614f36319098ae7693f2c
Karan Vaid <vaidkaran89@gmail.com>
2018-01-21 13:47:32 +0530

    wip uploading junit reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,4 +12,19 @@ class ReportsController < ApplicationController
       @report = JSON(res)
     end
   end
+
+  def create
+    res = RestClient.post "#{api_base_url}/upload/junit", create_report_params, auth_headers
+    if res.code==200
+      flash[:notice] = 'Report uploaded successfully'
+    else
+      flash[:notice] = 'Something went wrong. Could not upload report'
+    end
+    redirect_to reports_url(test_category_id: params[:test_category_id])
+  end
+
+  private
+  def create_report_params
+    {project_name: 'test project', test_category: 'unit tests', report_type: 'junit', report_format: 'xml', upload: params['xml_upload'].tempfile}
+  end
 end
